e0a139ef3457ddf729fb258580323c6dda02c2c6
Alisson Frota Soares <alissonfrotasoares@gmail.com>
2024-06-21 19:55:00 +0100

    add create blade

---
+++ resources/views/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Add Task')
+
+@section('content')
+    {{-- {{ $errors }} --}}
+    <form action="{{ route('tasks.store') }}" method="post">
+        @csrf
+        <div>
+            <label for="title">
+                Title
+                <input type="text" name="title" id="title" @error('title') aria-invalid="true" @enderror>
+                @error('title')
+                    <small id="invalid-helper">{{ $message }}</small>
+                @enderror
+            </label>
+        </div>
+        <div>
+            <label for="description">
+                Description
+                <textarea name="description" id="description" @error('description') aria-invalid="true" @enderror></textarea>
+                @error('description')
+                    <small id="invalid-helper">{{ $message }}</small>
+                @enderror
+            </label>
+        </div>
+        <div>
+            <label for="long_description">
+                Long Description
+                <textarea name="long_description" id="long_description" @error('description') aria-invalid="true" @enderror></textarea>
+                @error('long_description')
+                    <small id="invalid-helper">{{ $message }}</small>
+                @enderror
+            </label>
+        </div>
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
+    </form>
+@endsection
