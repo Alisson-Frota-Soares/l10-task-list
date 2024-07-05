18c16cf97bc1e0b7bc27ee4e9a43313d13356d77
Alisson Frota Soares <alissonfrotasoares@gmail.com>
2024-07-05 19:22:19 +0100

    add toggleComplete

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,4 +13,9 @@ class Task extends Model
     protected $fillable = ['title', 'description', 'long_description'];
     // protected $guarded = ['password', 'secret', 'example'];
 
+    public function toggleComplete() {
+        $this->completed = !$this->completed;
+        $this->save();
+    }
+
 }
