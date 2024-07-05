95246da1ecda880d5b0ac5aca41eee6aa970c907
Alisson Frota Soares <alissonfrotasoares@gmail.com>
2024-07-05 19:23:10 +0100

    add links and toggle complete

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title',$task->title)
+@section('title', $task->title)
 
 @section('content')
     <p>{{ $task->description }}</p>
@@ -12,7 +12,25 @@
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
 
-    <form action="{{route('tasks.destroy', $task->id )}}" method="POST">
+    <p>
+        @if ($task->completed)
+            Completed
+        @else
+            Not Completed
+        @endif
+    </p>
+
+    <div>
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+    </div>
+
+    <form action="{{ route('tasks.toggle-complete', $task->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <button type="submit"> Mark as {{ $task->completed ? 'not completed' : 'completed' }} </button>
+    </form>
+
+    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST">
         @csrf
         @method('DELETE')
         <button type="submit">Delete</button>
